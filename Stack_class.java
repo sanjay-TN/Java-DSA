c8e50f065de1e93ce93f23d7b302da5e03c7395f
Sanjay T N <Sanjaysanjaytn77@gmail.com>
2025-10-30 21:52:53 +0530

    Stack using Collection

---
+++ Stack_class.java
@@ -0,0 +1,30 @@
+package com.dsa;
+import java.util.Stack;
+
+public class Stack_class {
+	
+//	insert an element at bottom of the stack
+	public static void pushAtBottom(int data,Stack<Integer> s) {
+		if(s.isEmpty()) {
+			s.push(data);
+			return;
+		}
+		int top=s.pop();
+		pushAtBottom(data,s);
+		s.push(top);
+		
+	}
+	public static void main(String[] args) {
+		Stack<Integer> s=new Stack<>();
+		s.push(1);
+		s.push(2);
+		s.push(3);
+		pushAtBottom(4,s);
+		while(!s.isEmpty()) {
+			System.out.println(s.peek());
+			s.pop();
+		}
+		
+	}
+
+}
